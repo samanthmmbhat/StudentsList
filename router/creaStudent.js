4dc32779644a3414d72ebd17225b43e6d3da2c73
samanthmm <90998580+samanthmm@users.noreply.github.com>
2024-01-03 23:14:18 +0530

    get summary based date and name added

--- router/creaStudent.js
+++ router/creaStudent.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const route = express.Router();
-
 const Student = require('../models/Student');
 
 route.post('/',async(req,res)=>{
@@ -50,34 +49,16 @@ route.put('/:id',async(req,res)=>{
 
 route.get('/sumarry',async(req,res)=>{
     try {
-    //     const customers = Student.find();
-        
-    //     const dateArray = customers.map(customer => (customer.date ? customer.date.toISOString().split('T')[0] : null));
-
-    //     const invoiceCount = customers.length;
-    //     const totalStudent = customers.reduce((total, customer) =>customer, 0);
-    
-    //     const summary = {
-    //       date: dateArray,
-    //       name:totalStudent
-    //     };
-    // const save = await customers.save();
-
-    const { date } = req.query;
-
-    if (!date) {
-      return res.status(400).json({ error: 'Date is required.' });
-    }
-
-    const startDate = new Date(date);
-    const endDate = new Date(date);
-    endDate.setDate(endDate.getDate() + 1); // Next day
-
-    const students = await Student.find({
-      date: { $gte: startDate, $lt: endDate },
-    });
-
-    res.json(students);
+    const uniqueDates = await Student.distinct('date');
+   
+    const summary = await Promise.all(
+      uniqueDates.map(async (date) => {
+        const students = await Student.find({ date });
+        const studentNames = students.map((student) => student.name);
+        return { date, studentNames };
+      })
+    );
+    res.json(summary);
     }catch(error){
         res.status(500).json({error:error.message})
     }
